bc7dace0641fd1542019585e7c29bbcb658b2d78
Anna Ballot <annaballot@gmail.com>
2024-02-14 07:25:15 +0000

    Fix attraction mongo model

--- src/models/mongo/attraction.js
+++ src/models/mongo/attraction.js
@@ -3,9 +3,11 @@ import Mongoose from "mongoose";
 const { Schema } = Mongoose;
 
 const attractionSchema = new Schema({
-  title: String,
-  artist: String,
-  duration: Number,
+  name: String,
+  category: String,
+  description: String,
+  latitude: Number,
+  longitude: Number,
   listid: {
     type: Schema.Types.ObjectId,
     ref: "List",
